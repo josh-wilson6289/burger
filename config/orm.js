c36e1a4ebe4ff24a9b85cacd7b19a9df801516e0
Josh Wilson <joshua.wilson6289@gmail.com>
2020-10-20 13:32:51 -0500

    added functions to orm.  haven't tested yet

--- config/orm.js
+++ config/orm.js
@@ -7,7 +7,34 @@ const connection = require("./connection.js");
 // `updateOne()`
 
 const orm = {
+  selectAll: function(cb) {
+    let queryString = `SELECT * FROM burgers;`;
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(burger, cb) {
+    let queryString = `INSERT INTO burgers (burger_name) VALUES ('${burger}');`
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  updateOne: function(burgerId, condition, cb) {
+    let queryString = `UPDATE burgers SET devoured = ${condition} WHERE ${burgerId} = id;`
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
 
+      cb(result);
+    });
+  }
 };
 
 
