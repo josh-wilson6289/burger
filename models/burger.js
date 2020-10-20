b3503ca9dcf3d8365fdeeaeccecddfb00d8efe20
Josh Wilson <joshua.wilson6289@gmail.com>
2020-10-20 12:43:34 -0500

    initial commit with file structure

---
+++ models/burger.js
@@ -0,0 +1,14 @@
+const orm = require("../config/orm.js");
+
+// create the code that will call the ORM functions using burger specific input for the ORM.
+
+
+
+
+
+
+
+
+
+
+// Export at the end of the `burger.js` file.
